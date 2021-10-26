c98370de4c2a33ee7b9d780cb01a04a14cfbe73d
nir-adler <49394931+nir-adler@users.noreply.github.com>
2021-10-26 10:54:22 +0300

    Update mongoose.js

--- sector-14/remake-9/server/src/db/mongoose.js
+++ sector-14/remake-9/server/src/db/mongoose.js
@@ -1,6 +1,5 @@
-const mongoose=require('mongoose')
 
-const url='mongodb+srv://taskapp:Ride2021up2022@cluster0.ezxmq.mongodb.net/tracks?retryWrites=true&w=majority'
+
 
 mongoose.connect(url,{
     useNewUrlParser: true,
